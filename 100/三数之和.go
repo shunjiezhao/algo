45a1e6e1494c8d9868a55434f4a24253241904a8
shunjiezhao <939038111@qq.com>
2023-07-23 22:06:33 +0800

    weekend

--- 100/三数之和.go
+++ 100/三数之和.go
@@ -1,1 +1,30 @@
 package _00
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	n := len(nums)
+	ans := make([][]int, 0)
+	for i, v := range nums {
+		if i > 0 && nums[i-1] == v {
+			continue
+		}
+		target := -v
+		l, r := i+1, n-1
+		for l < r {
+			sum := nums[l] + nums[r]
+			if sum == target {
+				ans = append(ans, []int{nums[i], nums[l], nums[r]})
+			}
+			if sum >= target { // move big
+				for rval := nums[r]; l < r && nums[r] == rval; r-- {
+				}
+			} else {
+				for lval := nums[l]; l < r && nums[l] == lval; l++ {
+				}
+			}
+		}
+	}
+	return ans
+}
--- week/109/main.go
+++ week/109/main.go
@@ -1,74 +1,98 @@
 package main
 
-func removeTrailingZeros(num string) string {
-	n := len(num)
-	for i := n - 1; i > 0; i-- {
-		if num[i] != '0' {
-			break
-		}
-		num = num[:i]
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func isGood(nums []int) bool {
+	sort.Ints(nums)
+	cnt := make(map[int]int)
+	big := len(nums) - 1
+	if nums[len(nums)-1] != big {
+		return false
 	}
-	return num
+	for _, num := range nums {
+		cnt[num]++
+		if cnt[num] > 1 && num != big {
+			return false
+		}
+
+	}
+	return cnt[big] == 2
 }
 
-func differenceOfDistinctValues(grid [][]int) [][]int {
-	n, m := len(grid), len(grid[0]) // row, col
-	ans := make([][]int, n)
-	contain := func(i, j int) bool {
-		return i >= 0 && i < n && j >= 0 && j < m
+func sortVowels(s string) string {
+	cnt := make(map[byte]bool)
+	for _, i := range []byte{'a', 'e', 'i', 'o', 'u'} {
+		cnt[i] = true
+		cnt[i-'a'+'A'] = true
 	}
-	calc := func(i, j int) {
-		if !contain(i, j) {
-			return
-		}
-		help := func(delta int) int {
-			cnt := make(map[int]bool)
-			a := 0
-			for x, y := i+delta, j+delta; contain(x, y); x, y = x+delta, y+delta {
-				if _, ok := cnt[grid[x][y]]; ok {
-					continue
-				}
-				a++
-				cnt[grid[x][y]] = true
-			}
-			return a
-		}
-		ans[i][j] = abs(help(1) - help(-1))
-	}
-	for i := 0; i < n; i++ {
-		ans[i] = make([]int, m)
-		for j := 0; j < m; j++ {
-			calc(i, j)
+	fmt.Println(cnt)
+
+	idx := make(map[byte]int)
+	all := make([]int, 0)
+	for i := range s {
+		b := s[i]
+		if cnt[b] == true {
+			idx[b]++
+			all = append(all, i)
 		}
 	}
-	return ans
+	var k []byte
+	for key := range idx {
+		k = append(k, key)
+	}
+	sort.Slice(k, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+
+	t := []byte(s)
+	i := 0
+	for _, key := range k {
+		for j := 0; j < idx[key]; j++ {
+			t[all[i]] = key
+			i++
+		}
+
+	}
+	return string(t)
+}
+func maxScore(nums []int, x int) int64 {
+	n := len(nums)
+	dp := make([][2]int64, n)
+	getLow := func(i int) int {
+		return i & 1
+	}
+
+	dp[0][getLow(nums[0])] = int64(nums[0])
+
+	for i := 1; i < n; i++ {
+		low := getLow(nums[i])
+		Xlow := 1 ^ low
+
+		dp[i][low] = max(dp[i-1][Xlow]-int64(x), dp[i-1][low]) + int64(nums[i])
+		dp[i][Xlow] = dp[i-1][Xlow]
+	}
+
+	return max(dp[n-1][0], dp[n-1][1])
 }
 
-func abs(a int) int {
-	if a > 0 {
+func numberOfWays(n int, x int) int {
+	f := make([]int64, n+1)
+	f[0] = 1
+	for i := 1; i <= n; i++ {
+		pow := int64(math.Pow(float64(i), float64(x)))
+		for j := int64(n); j >= pow; j-- { // 注意 f[i][j] 表示 前 i 个数，和为 j 的方案数，而物品的集合有 {i^x | i ^ x < n }，然后我们就利用 01 背包进行选择即可
+			f[j] += f[j-pow]
+		}
+	}
+	return int(f[n] % (1e9 + 7))
+}
+func max(a, b int64) int64 {
+	if a > b {
 		return a
 	}
-	return -a
-
-}
-
-func minimumCost(s string) int64 {
-	n := len(s)
-	ans := int64(0)
-	for i := 1; i < n; i++ {
-		if s[i] != s[i-1] {
-			ans += int64(min(i, n-i))
-		}
-	}
-	return ans
-}
-func min(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
-}
-
-func main() {
-	println(minimumCost("010101"))
+	return b
 }
